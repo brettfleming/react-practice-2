de808865e6b1ead43eb9fe614f9afe22e0f98758
Brett Fleming <brettfleming015@gmail.com>
2021-11-01 21:24:57 -0500

    adding the event handler for adding todos

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import './App.css';
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import TodoList from './TodoList';
 
 
 function App() {
   const [todos, setTodos] = useState([{ id: 1, name: 'todo 1', complete: false}])
+  const todoNameRef = useRef(initialValue)
+  function handleAddTodo(e) {
+
+    
+  }
   return (
     <>
     <TodoList todos={todos}/>
-    <input type="text" />
-    <button>Add Todo</button>
+    <input ref={todoNameRef} type="text" />
+    <button onClick={handleAddTodo}>Add Todo</button>
     <button>Clear Complete</button>
     <div>0 left to do</div>
     </>
