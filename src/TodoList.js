6ebbddd801bae0a84fde4969e18843ae7064506f
Brett Fleming <brettfleming015@gmail.com>
2021-11-01 20:57:40 -0500

    getting the todo component up and running

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import todo from './Todo'
+
 export default function TodoList({todos}) {
     return (
-        <div>
-            {todos.length}
-            
-        </div>
+        todos.map(todo => {
+            return <Todo todo={todo} />
+        })
     )
 }
