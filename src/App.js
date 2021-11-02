77bc8a74ac79688203c91e7a98a0b89e54555967
Brett Fleming <brettfleming015@gmail.com>
2021-11-01 20:48:08 -0500

    adding usestate

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import React, { useState } from 'react'
 import TodoList from './TodoList';
 
 
 function App() {
+  const [todos, setTodos] = useState([])
   return (
     <>
     <TodoList />
